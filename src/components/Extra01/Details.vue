d73056d9983da2de2e3a2cebf9a49f7630b6255b
Ericson <ericson.takay@gmail.com>
2024-08-02 10:58:23 -0300

    Fim - Compartilhando dados globais; Inicio - Evitando Prop Drilling Parte 2;

---
+++ src/components/Extra01/Details.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button @click="meStore.getMe()">Get details</button>
+  </div>
+</template>
+
+<script setup>
+import { useMeStore} from "../../store/Extra01/me.js";
+
+const meStore = useMeStore()
+</script>
---
+++ src/components/Extra01/Preferences.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button @click="meStore.getMe()">Get preferences</button>
+  </div>
+</template>
+
+<script setup>
+import { useMeStore} from "../../store/Extra01/me.js";
+
+const meStore = useMeStore()
+</script>
